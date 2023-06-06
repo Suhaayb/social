cdb995b9a1f0d47914d9a03ae9996ed772e0f1fa
Suhaayb <1197145@student.roc-nijmegen.nl>
2023-06-06 13:32:35 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import { collection, getDocs, query } from 'firebase/firestore';
+import { db, signInWithGoogle } from './config/firebase';
+
+// import { auth, googleProvider } from './config/firebase';
+// import { signInWithPopup } from 'firebase/auth';
 
 function App() {
+  const [Posts, setPosts] = useState([]);
+  // const [userCedentials, setCredientials] = useState([]);
+
+  const haalDocumentenOp = () => {
+    const q = query(collection(db, 'Posts'));
+    getDocs(q).then((firebaseResponse) => {
+      const lijstVanDocumenten = firebaseResponse.docs.map((doc) => doc.data());
+      setPosts(lijstVanDocumenten);
+    });
+  };
+
+  // const loginWithGoogle = async () => {
+  //   const userCedentials = await signInWithPopup(auth, googleProvider);
+  //   setCredientials(userCedentials);
+  //   console.log(userCedentials);
+  // };
+
+  useEffect(() => {
+    haalDocumentenOp();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      {/* <div>
+        <button onClick={loginWithGoogle}>Login met Google</button>
+      </div> */}
+      {Posts.map((Posts) => (
+        <div>
+          <h1>{Posts.Title}</h1>
+          <p>{Posts.Description}</p>
+        </div>
+      ))}
+      <button class='login-with-google-btn' onClick={signInWithGoogle}>
+        Sign in with Google
+      </button>
+      <h1>{localStorage.getItem('name')}</h1>
+      <h1>{localStorage.getItem('email')}</h1>
     </div>
   );
 }
---
+++ src/config/firebase.js
@@ -0,0 +1,37 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyCksfrt_apwH4NK1KsZ_CuQX6wm1sE-_Oo',
+  authDomain: 'social-ec23f.firebaseapp.com',
+  projectId: 'social-ec23f',
+  storageBucket: 'social-ec23f.appspot.com',
+  messagingSenderId: '147144369588',
+  appId: '1:147144369588:web:3fd33bc7ff3e4d4e57fee1',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider(app);
+
+export const signInWithGoogle = () => {
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      const name = result.user.displayName;
+      const email = result.user.email;
+
+      localStorage.setItem('name', name);
+      localStorage.setItem('email', email);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
