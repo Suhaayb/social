f7fdb6e39d55b6b911c2249311a107b2399f0024
Wessel <appelhofwessel@gmail.com>
2023-06-19 11:26:52 +0200

    Login/Register

--- src/Login.js
+++ src/Login.js
@@ -2,18 +2,46 @@ import './App.css';
 import React from 'react';
 import { useState, useEffect } from 'react';
 import { collection, getDocs, query } from 'firebase/firestore';
-import { db, signInWithGoogle } from './config/firebase';
+import { db, signInWithGoogle, auth } from './config/firebase';
+import { signInWithEmailAndPassword } from 'firebase/auth';
 
 
-function Login() {
- 
+const Login = () => {
 
-  return (
-    <div className='App'>
-      <div className='Login-left'></div>
-      <div className='Login-right'></div>
-    </div>
-  );
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const signIn = (e) => {
+    e.preventDefault();
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        console.log(userCredential);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    };
+
+    return (
+      <div className='sign-in-container'>
+        <form onSubmit={signIn}>
+          <h1>Log In to your Account</h1>
+          <input
+            type='email'
+            placeholder='Enter your email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          ></input>
+          <input
+            type='password'
+            placeholder='Enter your password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          ></input>
+          <button type='submit'>Log In</button>
+        </form>
+      </div>
+    );
 }
 
 export default Login;
--- src/Register.js
+++ src/Register.js
@@ -2,17 +2,48 @@ import './App.css';
 import React from 'react';
 import { useState, useEffect } from 'react';
 import { collection, getDocs, query } from 'firebase/firestore';
-import { db, signInWithGoogle } from './config/firebase';
+import { db, signInWithGoogle, auth } from './config/firebase';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
 
 
-function Register() {
- 
 
-  return (
-    <div className='App'>
-      Register
-    </div>
-  );
-}
+const Register = () => {
+
+    
+        const [email, setEmail] = useState('');
+        const [password, setPassword] = useState('');
+      
+        const signUp = (e) => {
+          e.preventDefault();
+          createUserWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+              console.log(userCredential);
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        };
+
+        return (
+            <div className='sign-in-container'>
+              <form onSubmit={signUp}>
+                <h1>Create Account</h1>
+                <input
+                  type='email'
+                  placeholder='Enter your email'
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                ></input>
+                <input
+                  type='password'
+                  placeholder='Enter your password'
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                ></input>
+                <button type='submit'>Sign Up</button>
+              </form>
+            </div>
+          );
+        };
 
 export default Register;
