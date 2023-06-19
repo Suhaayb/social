5d0f5b96a7d5ce97dcf286bd5465b4efd0627418
Wessel <appelhofwessel@gmail.com>
2023-06-19 11:43:05 +0200

    css

--- src/App.js
+++ src/App.js
@@ -45,9 +45,26 @@ function App() {
       </button>
       <h1>{localStorage.getItem('name')}</h1>
       <h1>{localStorage.getItem('email')}</h1> */}
+      <ul>
+        <li>
+          <a href='/'>Home</a>
+        </li>
+
+        <li>
+          <a href='/search'>Search</a>
+        </li>
+
+        <li>
+          <a href='/login'>Login</a>
+        </li>
+
+        <li>
+          <a href='/register'>Register</a>
+        </li>
+      </ul>
       <Routes>
-        <Route path='login' element={<Login />} />
-        <Route path='register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
       </Routes>
     </>
   );
